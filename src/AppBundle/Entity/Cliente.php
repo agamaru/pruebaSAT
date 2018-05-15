f9875c3900079661de736288d63c537fc73d6920
agamaru <agamaru@gmail.com>
2018-05-15 19:37:23 +0200

    Añadida entidad Cliente

---
+++ src/AppBundle/Entity/Cliente.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elsiore
+ * Date: 15/05/18
+ * Time: 19:27
+ */
+
+namespace AppBundle\Entity;
+
+
+class Cliente
+{
+    private $id;
+
+    private $nombreCliente;
+
+    private $direccion;
+
+    private $telefono;
+
+    private $email;
+}
