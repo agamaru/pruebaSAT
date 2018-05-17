67b414e203a420f041d36578f1964dcd33c8f4c4
agamaru <agamaru@gmail.com>
2018-05-17 20:15:06 +0200

    Añadidos getters y setters a la entidad ConfiRed

--- src/AppBundle/Entity/ConfiRed.php
+++ src/AppBundle/Entity/ConfiRed.php
@@ -67,4 +67,123 @@ class ConfiRed
      * @var string
      */
     private $cliente;
+
+    /// Getters y setters
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDominio()
+    {
+        return $this->dominio;
+    }
+
+    /**
+     * @param string $dominio
+     * @return ConfiRed
+     */
+    public function setDominio($dominio)
+    {
+        $this->dominio = $dominio;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMascaraRed()
+    {
+        return $this->mascaraRed;
+    }
+
+    /**
+     * @param string $mascaraRed
+     * @return ConfiRed
+     */
+    public function setMascaraRed($mascaraRed)
+    {
+        $this->mascaraRed = $mascaraRed;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpFijaExt()
+    {
+        return $this->ipFijaExt;
+    }
+
+    /**
+     * @param string $ipFijaExt
+     * @return ConfiRed
+     */
+    public function setIpFijaExt($ipFijaExt)
+    {
+        $this->ipFijaExt = $ipFijaExt;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDns1()
+    {
+        return $this->dns1;
+    }
+
+    /**
+     * @param string $dns1
+     * @return ConfiRed
+     */
+    public function setDns1($dns1)
+    {
+        $this->dns1 = $dns1;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDns2()
+    {
+        return $this->dns2;
+    }
+
+    /**
+     * @param string $dns2
+     * @return ConfiRed
+     */
+    public function setDns2($dns2)
+    {
+        $this->dns2 = $dns2;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCliente()
+    {
+        return $this->cliente;
+    }
+
+    /**
+     * @param string $cliente
+     * @return ConfiRed
+     */
+    public function setCliente($cliente)
+    {
+        $this->cliente = $cliente;
+        return $this;
+    }
+
 }
