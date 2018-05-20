c364e562beb0987252a47b8b5f21d6b4dcc7a8f5
agamaru <agamaru@gmail.com>
2018-05-20 20:16:13 +0200

    Modificada relación de la entidad Software. Enlazada con Empresa (M:1)

--- src/AppBundle/Entity/Software.php
+++ src/AppBundle/Entity/Software.php
@@ -70,12 +70,12 @@ class Software
     private $tipo;
 
     /**
-     * @ORM\ManyToOne(targetEntity="Cliente")
+     * @ORM\ManyToOne(targetEntity="Empresa")
      * @ORM\JoinColumn(nullable=false)
      *
-     * @var Cliente
+     * @var Empresa
      */
-    private $cliente;
+    private $empresa;
 
     /// Getters y setters
 
@@ -196,20 +196,20 @@ class Software
     }
 
     /**
-     * @return Cliente
+     * @return Empresa
      */
-    public function getCliente()
+    public function getEmpresa()
     {
-        return $this->cliente;
+        return $this->empresa;
     }
 
     /**
-     * @param Cliente $cliente
+     * @param Empresa $empresa
      * @return Software
      */
-    public function setCliente($cliente)
+    public function setEmpresa($empresa)
     {
-        $this->cliente = $cliente;
+        $this->empresa = $empresa;
         return $this;
     }
 
